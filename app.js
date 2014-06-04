042ded40adc5ad02acf6b4e84a1751a51a4771bd
srgg6701 <srgg67@gmail.com>
2014-06-04 17:35:16 +0400

    Создан скелетон, подключены библиотеки. Те, что через bower (angular, angular-resource), почему-то глючат.

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+var app = angular.module('tApp',['ngResource'])
+  .config( function($routeProvider, $locationProvider){
+    $routeProvider
+        .when('/',{
+            templateUrl:'templates/default.html',
+            controller: 'defaultController'
+        })
+        .when('/contacts',{
+            templateUrl:'templates/contacts.html',
+            controller: 'contactsController'
+        })
+        .when('/test',{
+            templateUrl:'templates/test.html',
+            controller: 'testController'
+        })
+        .otherwise({
+            templateUrl:'templates/404.html',
+            controller: 'defaultController'
+        });
+    $locationProvider.html5Mode(true);
+});
---
+++ controllers/contactsController.js
@@ -0,0 +1,6 @@
+"use strict";
+
+app.controller('contactsController',
+    function contactsController($scope, $http){
+        console.log('Hello! Contacts controller comes here.');
+});
