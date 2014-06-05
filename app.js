1c871828d9e23c156716360c9a9d92eed95926cc
srgg6701 <srgg67@gmail.com>
2014-06-05 10:53:45 +0400

    Эксперимент со слэшем.

--- app.js
+++ app.js
@@ -28,15 +28,15 @@ var app = angular.module('tApp',[])
     for(var i in menu){
         var start=(menu[i]=='default')? '':menu[i];
         $routeProvider.when('/'+start,{
-            templateUrl:    'templates/'+menu[i]+'.html',
+            templateUrl:    '/templates/'+menu[i]+'.html',
             controller:     menu[i]+'Controller'
         });
     }
     $routeProvider.when('/index.html',{
-        templateUrl:    'templates/default.html',
+        templateUrl:    '/templates/default.html',
         controller:     'defaultController'
     }).otherwise({
-        templateUrl:    'templates/404.html',
+        templateUrl:    '/templates/404.html',
         controller:     'defaultController'
     });
     $locationProvider.html5Mode(true);
