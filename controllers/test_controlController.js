61db04e48ce04296e9a43672414ccd3a9631c429
srgg6701 <srgg67@gmail.com>
2014-06-12 00:54:52 +0400

    Использование jQuery в контроллере для плавной прокрутки; модификация стилей.

--- controllers/test_controlController.js
+++ controllers/test_controlController.js
@@ -4,7 +4,14 @@ app.controller('test_controlController',
     function test_controlController($scope,getFileContents){
         $scope.contents = getFileContents.getContents('test_control');
         $scope.contents.then(
-            function(event){
+            function(event){ // Resource object
                 console.dir(event);
         });
+        $scope.scrollTo = function(anchor){
+            var sTop = $('a[name="'+anchor+'"]').offset().top;
+            $('body').animate({scrollTop:sTop},300);
+        };
+        $scope.goTop = function(){
+            $('body').animate({scrollTop:0},300);
+        };
 });
