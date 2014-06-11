109ec4cd4b80314e279cb96280bc8fa570773923
srgg6701 <srgg67@gmail.com>
2014-06-11 21:27:47 +0400

    Сервис извлечения контента из JSON-файла подвергнут рефакторингу.

--- app.js
+++ app.js
@@ -47,21 +47,18 @@ var app = angular.module('tApp',['ngResource'])
 )
   .factory('getFileContents',
     function($resource, $q){
-        var resource = $resource('/data/contents/:id', {id:'@id'});
         return {
             getContents: function (param) { // http://stackoverflow.com/questions/24129421/can-i-pass-a-param-from-controller-into-service-in-angularjs
                 //return "paramId comes here: "+param;
                 var deffered = $q.defer(); // http://angular.ru/api/ng.$q
-                resource.get({
-                        id:param
-                    },
+                $resource('/data/contents/:filename',{ filename:param })
+                  .get(
                     function(event){
                         deffered.resolve(event);
                     },
                     function(response){
                         deffered.reject(response);
-                    }
-                );
+                    });
                 return deffered.promise;
             }
         }
