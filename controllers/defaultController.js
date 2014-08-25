fe59b2f2da79f38a516f5a7d488e130be055aa51
srgg6701 <srgg67@gmail.com>
2014-08-25 14:51:51 +0400

    Добавление анимированного подменю.

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -2,7 +2,11 @@
 
 app.controller('defaultController',
     function defaultController($scope,mainMenu){
-        console.dir(mainMenu);
+        //console.dir(mainMenu);
         $scope.menus=mainMenu;
 		//$scope.title="AngularJS the SuperHeroic tutorial";
+        $scope.toggleSubmenu=function(header){
+            //usefor: использование jquery
+            $(header).parent().find('ul').fadeToggle(300);
+        }
 });
