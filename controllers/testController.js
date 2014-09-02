4f21ff72f5b47d62f5891bb79155c751429f41ed
srgg6701 <srgg67@gmail.com>
2014-09-02 08:48:29 +0400

    Изменён логотип (размер/цвет шрифта).

--- controllers/testController.js
+++ controllers/testController.js
@@ -4,7 +4,8 @@ app.controller('testController',
     function testController($scope, $http){
         console.log('Hello! Test controller comes here.');
         $scope.$watch('temp_model', function(newValue, oldValue) {
-            console.log('oldValue: '+oldValue+', newValue: '+newValue);
+            console.log('oldValue: %c'+oldValue, 'color:brown');
+            console.log('newValue: %c'+newValue,'color: violet');
             //update the DOM with newValue
         });
     });
